812d436001df4ccb9a1f0a24f002543ab46e41c2
domaincrisman <cmanzanore@gmail.com>
2023-02-22 19:47:25 +0100

    review sintax with rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get '/products', to:'products#index'
-  get '/products/:id', to:'products#show', as: :product
+  get '/products', to: 'products#index'
+  get '/products/:id', to: 'products#show', as: :product
 end
