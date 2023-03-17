0ff652994f07e0f9717199e12d0c0e26ba97e8f6
domaincrisman <cmanzanore@gmail.com>
2023-03-17 14:47:34 +0100

    Save products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,8 +8,21 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
- 
+
   def new
     @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render 'new'
+    end
+  end
+
+  def product_params
+    params.require(:product).permit(:title, :description, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  post '/products', to: 'products#create'
   get '/products/new', to: 'products#new', as: :new_product
   get '/products', to: 'products#index'
   get '/products/:id', to: 'products#show', as: :product
