018c37c7bf5f86b1fb351e2b5aa592428b8554f1
domaincrisman <cmanzanore@gmail.com>
2023-03-20 18:16:21 +0100

    add product new validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
 end
