bf204b01ffbe8d622d5a6b0eff9a38b53d2b822b
domaincrisman <cmanzanore@gmail.com>
2023-03-27 22:09:41 +0200

    update product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  patch '/products/:id', to: 'products#update'
   post '/products', to: 'products#create'
   get '/products/new', to: 'products#new', as: :new_product
   get '/products', to: 'products#index'
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -51,8 +51,29 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test 'render an edit product form' do
     get edit_product_path(products(:ps4))
-    
+
     assert_response :success
     assert_select 'form'
+  end
+
+  test 'allow to update a product' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: 165
+      }
+    }
+
+    assert_redirected_to products_path
+    assert_equal flash[:notice], 'Tu producto se ha actualizado correctamente'
+  end
+
+  test 'does not allow to update a product with an invalid field' do
+    patch product_path(products(:ps4)), params: {
+      product: {
+        price: nil
+      }
+    }
+
+    assert_response :unprocessable_entity
   end
 end
