298cb88fe04235c5eef2ace7fdece620a316597b
domaincrisman <cmanzanore@gmail.com>
2023-03-30 19:48:29 +0200

    our first refactor

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,11 +2,11 @@
 
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
+    @products = Product.all.with_attached_photo
   end
 
   def show
-    @product = Product.find(params[:id])
+    product
   end
 
   def new
@@ -23,13 +23,11 @@ class ProductsController < ApplicationController
   end
 
   def edit
-    @product = Product.find(params[:id])
+    product
   end
 
   def update
-    @product = Product.find(params[:id])
-
-    if @product.update(product_params)
+    if product.update(product_params)
       redirect_to products_path, notice: 'Tu producto se ha actualizado correctamente'
     else
       render 'edit', status: :unprocessable_entity
@@ -37,15 +35,18 @@ class ProductsController < ApplicationController
   end
 
   def destroy
-    @product = Product.find(params[:id])
-    @product.destroy
-    
+    product.destroy
+
     redirect_to products_path, notice: 'Tu producto se ha eliminado correctamente', status: :see_other
   end
 
   private
 
   def product_params
-    params.require(:product).permit(:title, :description, :price)
+    params.require(:product).permit(:title, :description, :price, :photo)
+  end
+
+  def product
+    @product = Product.find(params[:id])
   end
 end
