9d147fdeb7e285c7cc15efdc178175276354cbc2
domaincrisman <cmanzanore@gmail.com>
2023-05-04 20:30:59 +0200

    looking for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+  include PgSearch::Model
+  pg_search_scope :search_full_text, against: {
+    title: 'A',
+    description: 'B'
+  }
   has_one_attached :photo
   
   validates :title, presence: true
