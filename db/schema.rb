8f8dd68a41b4cf13752bc29720188fdef9594224
domaincrisman <cmanzanore@gmail.com>
2023-05-11 16:16:55 +0200

    Update Rails y Ruby + Bug Fixing

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_18_171806) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_11_124838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_18_171806) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
