93ca2a93fedcddb99c21d4ddde94a8bd487ff033
domaincrisman <cmanzanore@gmail.com>
2024-02-15 17:19:22 +0100

    video 41: quitando favoritos

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@
 
 class Product < ApplicationRecord
   include PgSearch::Model
+  include Favoritable
   pg_search_scope :search_full_text, against: {
     title: 'A',
     description: 'B'
@@ -19,11 +20,11 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
 
-  has_many :favorites, dependent: :destroy
   belongs_to :category
   belongs_to :user, default: -> { Current.user }
 
   def owner?
     user_id == Current.user&.id
   end
+
 end
