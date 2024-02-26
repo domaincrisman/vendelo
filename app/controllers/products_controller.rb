e63b7ece9a2509e464d94e070915fbadf08d839e
domaincrisman <cmanzanore@gmail.com>
2024-02-26 02:46:48 +0100

    video 54: websockets con action cable

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,6 +32,7 @@ class ProductsController < ApplicationController
   def update
     authorize! product
     if product.update(product_params)
+      notify_all_users
       redirect_to products_path, notice: t('.updated')
     else
       render 'edit', status: :unprocessable_entity
@@ -57,5 +58,14 @@ class ProductsController < ApplicationController
 
   def product
     @product ||= Product.find(params[:id])
+  end
+
+  def notify_all_users
+    ActionCable.server.broadcast(
+      "product_#{product.id}",
+      {
+        action: "updated"
+      }
+    )
   end
 end
